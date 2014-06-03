1d9b915787a9ca75bff239518b602c701debf872
Izzy Leung <izzyliang@gmail.com>
2014-06-03 22:18:00 +0800

    Move Application class to the root package

---
+++ ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/ZhihuDailyPurifyApplication.java
@@ -0,0 +1,43 @@
+package io.github.izzyleung.zhihudailypurify;
+
+import android.app.Application;
+import android.content.Context;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import io.github.izzyleung.zhihudailypurify.db.DailyNewsDataSource;
+
+public final class ZhihuDailyPurifyApplication extends Application {
+    private static ZhihuDailyPurifyApplication applicationContext;
+    private DailyNewsDataSource dataSource;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext = this;
+
+        initImageLoader(getApplicationContext());
+        dataSource = new DailyNewsDataSource(getApplicationContext());
+        dataSource.open();
+    }
+
+    public static void initImageLoader(Context context) {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+
+        ImageLoader.getInstance().init(config);
+    }
+
+    public static ZhihuDailyPurifyApplication getInstance() {
+        return applicationContext;
+    }
+
+    public DailyNewsDataSource getDataSource() {
+        return dataSource;
+    }
+}

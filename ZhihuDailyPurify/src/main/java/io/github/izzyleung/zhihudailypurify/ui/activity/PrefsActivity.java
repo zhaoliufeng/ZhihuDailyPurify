ed2dc4a5371b4987b5331ec840a2de040f3ebab7
Izzy Leung <izzyliang@gmail.com>
2014-06-05 12:05:06 +0800

    Use PreferenceFragment

--- ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/ui/activity/PrefsActivity.java
+++ ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/ui/activity/PrefsActivity.java
@@ -1,75 +1,18 @@
 package io.github.izzyleung.zhihudailypurify.ui.activity;
 
-import android.app.Dialog;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.view.View;
-import android.view.Window;
-import android.widget.TextView;
-import io.github.izzyleung.zhihudailypurify.R;
+import android.support.v4.app.FragmentActivity;
+import io.github.izzyleung.zhihudailypurify.ui.fragment.PrefsFragment;
 
-@SuppressWarnings("deprecation")
-public class PrefsActivity extends PreferenceActivity implements Preference.OnPreferenceClickListener {
+public class PrefsActivity extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.prefs);
 
-        //noinspection ConstantConditions
-        findPreference("about").setOnPreferenceClickListener(this);
-
-        PackageManager pm = getPackageManager();
-        try {
-            if (pm != null) {
-                pm.getPackageInfo("com.zhihu.android", PackageManager.GET_ACTIVITIES);
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            //noinspection ConstantConditions
-            ((PreferenceCategory) findPreference("settings_settings")).removePreference(findPreference("using_client?"));
-        }
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        //noinspection ConstantConditions
-        if (preference.getKey().equals("about")) {
-            showApacheLicenseDialog();
-            return true;
-        }
-        return false;
-    }
-
-    private void showApacheLicenseDialog() {
-        final Dialog apacheLicenseDialog = new Dialog(this);
-        apacheLicenseDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        apacheLicenseDialog.setCancelable(true);
-        apacheLicenseDialog.setContentView(R.layout.dialog_apache_license);
-
-        TextView textView = (TextView) apacheLicenseDialog.findViewById(R.id.dialog_text);
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(getString(R.string.licences_header));
-
-        String[] basedOnProjects = getResources().getStringArray(R.array.apache_licensed_projects);
-
-        for (String str : basedOnProjects) {
-            sb.append("• ").append(str).append("\n");
-        }
-
-        sb.append("\n").append(getString(R.string.licenses_subheader));
-        textView.setText(sb.toString());
-
-        apacheLicenseDialog.findViewById(R.id.close_dialog_button)
-                .setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        apacheLicenseDialog.dismiss();
-                    }
-                });
-
-        apacheLicenseDialog.show();
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new PrefsFragment())
+                .commit();
     }
 }

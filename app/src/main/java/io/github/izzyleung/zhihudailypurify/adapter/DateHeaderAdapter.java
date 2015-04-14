8b6ea80fb7e82567d549e26227ec083dd12f876a
Izzy Leung <izzyliang@gmail.com>
2015-04-14 00:01:14 -0400

    Delete unused

---
+++ app/src/main/java/io/github/izzyleung/zhihudailypurify/adapter/DateHeaderAdapter.java
@@ -0,0 +1,73 @@
+package io.github.izzyleung.zhihudailypurify.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.eowise.recyclerview.stickyheaders.StickyHeadersAdapter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import io.github.izzyleung.zhihudailypurify.R;
+import io.github.izzyleung.zhihudailypurify.bean.DailyNews;
+import io.github.izzyleung.zhihudailypurify.support.Constants;
+
+public class DateHeaderAdapter implements StickyHeadersAdapter<DateHeaderAdapter.HeaderViewHolder> {
+    private List<DailyNews> newsList;
+    private SimpleDateFormat simpleDateFormat;
+
+    public DateHeaderAdapter(List<DailyNews> newsList) {
+        this.newsList = newsList;
+    }
+
+    public void setNewsList(List<DailyNews> newsList) {
+        this.newsList = newsList;
+    }
+
+    @Override
+    public HeaderViewHolder onCreateViewHolder(ViewGroup parent) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.date_sticky_header, parent, false);
+
+        simpleDateFormat = new SimpleDateFormat(context.getString(R.string.display_format));
+
+        return new HeaderViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(HeaderViewHolder viewHolder, int position) {
+        Calendar calendar = Calendar.getInstance();
+
+        try {
+            calendar.setTime(Constants.Date.simpleDateFormat.parse(newsList.get(position).getDate()));
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+        } catch (ParseException ignored) {
+
+        }
+
+        viewHolder.title.setText(simpleDateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+        return newsList.get(position).getDate().hashCode();
+    }
+
+    public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+}
--- app/src/main/java/io/github/izzyleung/zhihudailypurify/adapter/NewsAdapter.java
+++ app/src/main/java/io/github/izzyleung/zhihudailypurify/adapter/NewsAdapter.java
@@ -1,14 +1,22 @@
 package io.github.izzyleung.zhihudailypurify.adapter;
 
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -22,13 +30,10 @@ import java.util.List;
 
 import io.github.izzyleung.zhihudailypurify.R;
 import io.github.izzyleung.zhihudailypurify.bean.DailyNews;
-import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import io.github.izzyleung.zhihudailypurify.support.Check;
 
-public final class NewsAdapter extends BaseAdapter implements StickyListHeadersAdapter {
-    private LayoutInflater mInflater;
-
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.CardViewHolder> {
     private List<DailyNews> newsList;
-    private List<String> dateResultList;
 
     private ImageLoader imageLoader = ImageLoader.getInstance();
     private DisplayImageOptions options = new DisplayImageOptions.Builder()
@@ -41,16 +46,14 @@ public final class NewsAdapter extends BaseAdapter implements StickyListHeadersA
             .build();
     private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
 
-    public NewsAdapter(Context context) {
-        this.mInflater = LayoutInflater.from(context);
+    public NewsAdapter(List<DailyNews> newsList) {
+        this.newsList = newsList;
+
+        setHasStableIds(true);
     }
 
     public void setNewsList(List<DailyNews> newsList) {
         this.newsList = newsList;
-    }
-
-    public void setDateResultList(List<String> dateResultList) {
-        this.dateResultList = dateResultList;
     }
 
     public void updateNewsList(List<DailyNews> newsList) {
@@ -58,51 +61,44 @@ public final class NewsAdapter extends BaseAdapter implements StickyListHeadersA
         notifyDataSetChanged();
     }
 
-    public void updateDateResultList(List<String> dateResultList) {
-        setDateResultList(dateResultList);
-        notifyDataSetChanged();
-    }
+    @Override
+    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final Context context = parent.getContext();
 
-    public void updateContents(List<DailyNews> newsList, List<String> dateResultList) {
-        updateNewsList(newsList);
-        updateDateResultList(dateResultList);
+        View itemView = LayoutInflater
+                .from(context)
+                .inflate(R.layout.news_list_item, parent, false);
+
+        return new CardViewHolder(itemView, new CardViewHolder.ClickResponseListener() {
+            @Override
+            public void onWholeClick(int position) {
+                browseOrShare(context, position, true);
+            }
+
+            @Override
+            public void onOverflowClick(View v, final int position) {
+                PopupMenu popup = new PopupMenu(context, v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.contextual_news_list, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_share_url:
+                                browseOrShare(context, position, false);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
     }
 
     @Override
-    public int getCount() {
-        return newsList == null ? 0 : newsList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @SuppressLint("InflateParams")
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        CardViewHolder holder;
-        if (convertView == null) {
-            holder = new CardViewHolder();
-            convertView = mInflater.inflate(R.layout.news_list_item, null);
-
-            assert convertView != null;
-            holder.newsImage = (ImageView) convertView.findViewById(R.id.thumbnail_image);
-            holder.dailyTitle = (TextView) convertView.findViewById(R.id.daily_title);
-            holder.questionTitle = (TextView) convertView.findViewById(R.id.question_title);
-
-            convertView.setTag(holder);
-        } else {
-            holder = (CardViewHolder) convertView.getTag();
-        }
-
-        DailyNews dailyNews = new DailyNews(newsList.get(position));
-
+    public void onBindViewHolder(final CardViewHolder holder, final int position) {
+        final DailyNews dailyNews = new DailyNews(newsList.get(position));
         imageLoader.displayImage(dailyNews.getThumbnailUrl(), holder.newsImage, options, animateFirstListener);
 
         if (dailyNews.isMulti()) {
@@ -113,44 +109,132 @@ public final class NewsAdapter extends BaseAdapter implements StickyListHeadersA
             holder.questionTitle.setText(dailyNews.getQuestionTitle());
             holder.dailyTitle.setText(dailyNews.getDailyTitle());
         }
-
-        return convertView;
     }
 
     @Override
-    public long getHeaderId(int position) {
-        return dateResultList.get(position).hashCode();
+    public int getItemCount() {
+        return newsList.size();
     }
 
-    @SuppressLint("InflateParams")
     @Override
-    public View getHeaderView(int position, View convertView, ViewGroup parent) {
-        HeaderViewHolder holder;
-        if (convertView == null) {
-            holder = new HeaderViewHolder();
-            convertView = mInflater.inflate(R.layout.search_result_list_header, null);
+    public long getItemId(int position) {
+        return newsList.get(position).hashCode();
+    }
 
-            assert convertView != null;
-            holder.headerTitle = (TextView)
-                    convertView.findViewById(R.id.header_title);
+    private void browseOrShare(final Context context, int position, final boolean browse) {
+        final DailyNews dailyNews = newsList.get(position);
+        if (dailyNews.isMulti()) {
+            String[] questionTitles = dailyNews.getQuestionTitleList()
+                    .toArray(new String[dailyNews.getQuestionTitleList().size()]);
 
-            convertView.setTag(holder);
+            new AlertDialog.Builder(context)
+                    .setTitle(dailyNews.getDailyTitle())
+                    .setItems(questionTitles, makeDialogListener(context, browse, dailyNews))
+                    .show();
         } else {
-            holder = (HeaderViewHolder) convertView.getTag();
+            if (browse) {
+                goToZhihu(context, dailyNews.getQuestionUrl());
+            } else {
+                String questionTitle = dailyNews.getQuestionTitle(),
+                        questionUrl = dailyNews.getQuestionUrl();
+
+                share(context, questionTitle, questionUrl);
+            }
+        }
+    }
+
+    private DialogInterface.OnClickListener makeDialogListener(final Context context, final boolean browse, final DailyNews dailyNews) {
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (browse) {
+                    goToZhihu(context, dailyNews.getQuestionUrlList().get(which));
+                } else {
+                    String questionTitle, questionUrl;
+
+                    if (dailyNews.isMulti()) {
+                        questionTitle = dailyNews.getQuestionTitleList().get(which);
+                        questionUrl = dailyNews.getQuestionUrlList().get(which);
+                    } else {
+                        questionTitle = dailyNews.getQuestionTitle();
+                        questionUrl = dailyNews.getQuestionUrl();
+                    }
+
+                    share(context, questionTitle, questionUrl);
+                }
+            }
+        };
+    }
+
+    private void goToZhihu(Context context, String url) {
+        if (!PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean("using_client?", false)) {
+            openUsingBrowser(context, url);
+        } else if (Check.isZhihuClientInstalled()) {
+            //Open using Zhihu's official client
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            browserIntent.setPackage("com.zhihu.android");
+            context.startActivity(browserIntent);
+        } else {
+            openUsingBrowser(context, url);
+        }
+    }
+
+    private void openUsingBrowser(Context context, String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+        if (Check.isIntentSafe(browserIntent)) {
+            context.startActivity(browserIntent);
+        } else {
+            Toast.makeText(context, context.getString(R.string.no_browser), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void share(Context context, String questionTitle, String questionUrl) {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.setType("text/plain");
+        //noinspection deprecation
+        share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        share.putExtra(Intent.EXTRA_TEXT, questionTitle + " " + questionUrl + " 分享自知乎网");
+        context.startActivity(Intent.createChooser(share, context.getString(R.string.share_to)));
+    }
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ImageView newsImage;
+        public TextView questionTitle;
+        public TextView dailyTitle;
+        public ImageView overflow;
+
+        private ClickResponseListener mClickResponseListener;
+
+        public CardViewHolder(View v, ClickResponseListener clickResponseListener) {
+            super(v);
+
+            this.mClickResponseListener = clickResponseListener;
+
+            newsImage = (ImageView) v.findViewById(R.id.thumbnail_image);
+            questionTitle = (TextView) v.findViewById(R.id.question_title);
+            dailyTitle = (TextView) v.findViewById(R.id.daily_title);
+            overflow = (ImageView) v.findViewById(R.id.card_share_overflow);
+
+            v.setOnClickListener(this);
+            overflow.setOnClickListener(this);
         }
 
-        holder.headerTitle.setText(dateResultList.get(position));
-        return convertView;
-    }
+        @Override
+        public void onClick(View v) {
+            if (v == overflow) {
+                mClickResponseListener.onOverflowClick(v, getAdapterPosition());
+            } else {
+                mClickResponseListener.onWholeClick(getAdapterPosition());
+            }
+        }
 
-    private final static class CardViewHolder {
-        ImageView newsImage;
-        TextView questionTitle;
-        TextView dailyTitle;
-    }
+        public interface ClickResponseListener {
+            void onWholeClick(int position);
 
-    private final static class HeaderViewHolder {
-        TextView headerTitle;
+            void onOverflowClick(View v, int position);
+        }
     }
 
     private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {

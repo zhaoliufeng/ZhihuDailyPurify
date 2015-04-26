398822ff483e439d9ed2620d6f059e1560faca0e
Izzy Leung <izzyliang@gmail.com>
2015-04-26 06:48:46 -0400

    Reformat code, use LF as line breaks

--- app/src/main/java/io/github/izzyleung/zhihudailypurify/ui/activity/PrefsActivity.java
+++ app/src/main/java/io/github/izzyleung/zhihudailypurify/ui/activity/PrefsActivity.java
@@ -1,8 +1,6 @@
 package io.github.izzyleung.zhihudailypurify.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import io.github.izzyleung.zhihudailypurify.R;
 import io.github.izzyleung.zhihudailypurify.ui.fragment.PrefsFragment;
